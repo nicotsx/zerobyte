fdeab9f7fe0fedeefc4297ece0a4e5dbeca9946c
Nicolas Meienberger <github@thisprops.com>
2025-12-03 21:20:50 +0100

    chore: debug db queries

--- app/server/db/db.ts
+++ app/server/db/db.ts
@@ -6,11 +6,20 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { DATABASE_URL } from "../core/constants";
 import * as schema from "./schema";
 import fs from "node:fs/promises";
+import { logger } from "../utils/logger";
 
 await fs.mkdir(path.dirname(DATABASE_URL), { recursive: true });
 
 const sqlite = new Database(DATABASE_URL);
-export const db = drizzle({ client: sqlite, schema });
+export const db = drizzle({
+	client: sqlite,
+	schema,
+	logger: {
+		logQuery(query, params) {
+			logger.debug(`[Drizzle] ${query} -- [${params.join(",")}]`);
+		},
+	},
+});
 
 export const runDbMigrations = () => {
 	let migrationsFolder = path.join("/app", "assets", "migrations");
