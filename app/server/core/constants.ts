53ee9dc0c045294f6968fe02d42042211033fafc
Nicolas Meienberger <github@thisprops.com>
2025-12-18 22:28:37 +0100

    refactor: add default excludes to backups
    - To avoid accidentaly backing up a repository to itself
    - To avoid accidentaly backing up zerobyte's internal data

--- app/server/core/constants.ts
+++ app/server/core/constants.ts
@@ -4,4 +4,10 @@ export const REPOSITORY_BASE = "/var/lib/zerobyte/repositories";
 export const DATABASE_URL = "/var/lib/zerobyte/data/ironmount.db";
 export const RESTIC_PASS_FILE = "/var/lib/zerobyte/data/restic.pass";
 
+export const DEFAULT_EXCLUDES = [
+	DATABASE_URL,
+	RESTIC_PASS_FILE,
+	"/var/lib/zerobyte/repositories/**", // To avoid circular backups
+];
+
 export const REQUIRED_MIGRATIONS = ["v0.14.0"];
