21231759ab71d72296d15111683e151f81c673f1
Nicolas Meienberger <github@thisprops.com>
2025-12-21 15:03:10 +0100

    fix: migration using 1 instead of true

--- app/drizzle/0023_special_thor.sql
+++ app/drizzle/0023_special_thor.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `backup_schedules_table` ADD `one_file_system` integer DEFAULT false NOT NULL;
+ALTER TABLE `backup_schedules_table` ADD `one_file_system` integer DEFAULT false NOT NULL;--> statement-breakpoint
 UPDATE `backup_schedules_table` SET `one_file_system` = true;
