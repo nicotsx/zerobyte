df34dc759771c5456fa9588206601b882ec50d87
Nicolas Meienberger <github@thisprops.com>
2026-02-13 20:20:16 +0100

    refactor: add nitro bootstrap plugin to ensure app is started before first call

--- app/server.ts
+++ app/server.ts
@@ -1,23 +1,16 @@
 import { logger } from "./server/utils/logger";
 import { shutdown } from "./server/modules/lifecycle/shutdown";
 import { runCLI } from "./server/cli";
+import { bootstrapApplication } from "./server/modules/lifecycle/bootstrap";
 import { createStartHandler, defaultStreamHandler, defineHandlerCallback } from "@tanstack/react-start/server";
 import { createServerEntry } from "@tanstack/react-start/server-entry";
-import { initAuth } from "~/server/lib/auth";
-import { setSchema } from "./server/db/db";
-import * as schema from "./server/db/schema";
-import { toMessage } from "./server/utils/errors";
 
 const cliRun = await runCLI(Bun.argv);
 if (cliRun) {
 	process.exit(0);
 }
 
-setSchema(schema);
-await initAuth().catch((err) => {
-	logger.error(`Error initializing auth: ${toMessage(err)}`);
-	throw err;
-});
+await bootstrapApplication();
 
 const customHandler = defineHandlerCallback((ctx) => {
 	return defaultStreamHandler(ctx);
