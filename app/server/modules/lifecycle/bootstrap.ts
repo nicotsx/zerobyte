1efc4c3328aaf33ee5c1913b70794ddcb9c9f19c
Nicolas Meienberger <github@thisprops.com>
2026-02-13 21:08:58 +0100

    refactor: remove proxy pattern for db and auth
    Since we migrated away from rr this is not needed anymore as the bundler
    correctly split chunks

--- app/server/modules/lifecycle/bootstrap.ts
+++ app/server/modules/lifecycle/bootstrap.ts
@@ -1,25 +1,10 @@
-import * as schema from "../../db/schema";
-import { runDbMigrations, setSchema } from "../../db/db";
+import { runDbMigrations } from "../../db/db";
 import { runMigrations } from "./migrations";
 import { startup } from "./startup";
-import { initAuth } from "../../lib/auth";
-import { logger } from "../../utils/logger";
-import { toMessage } from "../../utils/errors";
 
 let bootstrapPromise: Promise<void> | undefined;
 
-export const initModules = async () => {
-	setSchema(schema);
-
-	await initAuth().catch((err) => {
-		logger.error(`Error initializing auth: ${toMessage(err)}`);
-		throw err;
-	});
-}
-
 const runBootstrap = async () => {
-	await initModules();
-
 	runDbMigrations();
 	await runMigrations();
 	await startup();
