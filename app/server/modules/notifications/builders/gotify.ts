727999b4c8e5e603ded960e6de0d565fe546a257
Nicolas Meienberger <github@thisprops.com>
2026-02-23 20:39:28 +0100

    fix: add DisableTLS param if server is http
    Closes #561

--- app/server/modules/notifications/builders/gotify.ts
+++ app/server/modules/notifications/builders/gotify.ts
@@ -5,11 +5,22 @@ export const buildGotifyShoutrrrUrl = (config: Extract<NotificationConfig, { typ
 	const hostname = url.hostname;
 	const port = url.port ? `:${url.port}` : "";
 	const path = config.path ? `/${config.path.replace(/^\/+|\/+$/g, "")}` : "";
+	const disableTLS = url.protocol === "http:";
 
 	let shoutrrrUrl = `gotify://${hostname}${port}${path}/${config.token}`;
 
+	const params = new URLSearchParams();
+
+	if (disableTLS) {
+		params.set("DisableTLS", "true");
+	}
+
 	if (config.priority !== undefined) {
-		shoutrrrUrl += `?priority=${config.priority}`;
+		params.set("priority", String(config.priority));
+	}
+
+	if (params.toString()) {
+		shoutrrrUrl += `?${params.toString()}`;
 	}
 
 	return shoutrrrUrl;
